50ea31aa227e030b0a27895638f5b1c0e07c74a1
Cartagena Anthony <anthonym.cartagena@gmail.com>
2019-01-03 16:15:55 -0500

    added todo list, toothTimer button, and modal for user questions

--- public/app.js
+++ public/app.js
@@ -1,4 +1,28 @@
+
+
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+    $('.modal').modal();
+    $('select').formSelect();
+
+
+    if(localStorage.getItem('userMorningNumber')===null){
+        console.log("there is no user MorningNumber here");
+        $("#userQuestions").modal("open");
+    }else{
+        console.log("user MorningNumber found!: "+localStorage.getItem('userMorningNumber'));
+    }
 });
+
+$('#toothTimerButton').on('click',function(){
+    console.log('start timer boi!')
+})
+
+
+$('.todos').on('click','a',function(){
+    console.log('get rid of to-do item!')
+    console.log(event.target.innerHTML)
+    console.log(event.srcElement)
+    $('.todos').append("<li class='collection-item'>"+event.target.innerHTML+"</li>")
+})
