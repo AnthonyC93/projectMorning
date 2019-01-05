8478c295d0376817c1891be433703be784a7e1a5
acarta <anthonym.cartagena@gmail.com>
2019-01-05 14:23:56 -0500

    added ORM/Controllers basic logic. Need to refine and connect to make server

---
+++ models/todos.js
@@ -0,0 +1,23 @@
+//import orm.js
+var orm = require('../config/orm');
+
+//create the code that will call the ORM functions using task specific input for the ORM.
+var todoList={
+    all:function(callBack){
+        orm.selectAll(function(response){
+            callBack(response);
+        })
+    },
+
+    update:function(newValue,userNumber){
+        orm.updateOne(newValue,userNumber);
+    },
+
+    add:function(burgerName){
+        orm.insertOne(burgerName)
+    }
+}
+
+
+//export   
+module.exports=burger;
