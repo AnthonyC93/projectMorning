037c161bce2ca923733cc97c8a274e6695cfd2a4
Andrew Neves <andrewneves@Andrews-MacBook-Pro.local>
2019-01-07 21:18:57 -0500

    pushing 1/7

--- public/app.js
+++ public/app.js
@@ -13,59 +13,101 @@ $(document).ready(function(){
     }else{
         console.log("user MorningNumber found!: "+localStorage.getItem('userMorningNumber'));
     }
+
+    
+    // var number =120
+    // var intervalId;
+
+    // $("#toothTimerButton").on("click",function(){
+    // $("#toothTimerButton").html(number);
+    // console.log("start timer boi")
+    // //  Execute the run function.
+    // run();
+    // })
+
+    // function run() {
+
+    //     var number =120
+    //     var intervalId;
+
+    //     console.log("function start") 
+
+    //     clearInterval(intervalId);
+    //     intervalId = setInterval(decrement, 1000);
+    //     console.log("function running")
+    //     // $("#toothTimerButton").html(number);
+    // }
+    
+var interval;
+function countdown() {
+ clearInterval(interval);
+ interval = setInterval( function() {
+     var timer = $("#toothTimerButton").html();
+     timer = timer.split(':');
+     var minutes = timer[0];
+     var seconds = timer[1];
+     seconds -= 1;
+     if (minutes < 0) return;
+     else if (seconds < 0 && minutes != 0) {
+         minutes -= 1;
+         seconds = 59;
+     }
+     else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+     $("#toothTimerButton").html(minutes + ':' + seconds);
+
+     if (minutes == 0 && seconds == 0) clearInterval(interval);
+ }, 1000);
+}
+
+$("#toothTimerButton").click(function () {
+ $("#toothTimerButton").text("2:00");
+ countdown();
 });
 
-$('#toothTimerButton').on('click',function(){
-    console.log('start timer boi!')
-    
-    var number = 120000;
-    //  Variable that will hold our interval ID when we execute
-    //  the "run" function
-    var intervalId;
-//  When the stop button gets clicked, run the stop function.
-    $("#stop").on("click", stop);
- //  When the resume button gets clicked, execute the run function.
-    $("#resume").on("click", run);
-    //  The run function sets an interval
-    //  that runs the decrement function once a second.
-    //  *****BUG FIX******** 
-    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 120000);
-    }
+//$('#js-resetTimer').click(function () {
+ //$("#toothTimerButton").text("2:00");
+ //clearInterval(interval);
+//});
+        
+   
+   //     }
+  //  }
 
     //  The decrement function.
-    function decrement() {
+    // function decrement() {
 
-      //  Decrease number by one.
-      number--;
+    //     //  Decrease number by one.
+    //     number--;
+    //     formattedNumber = convertMS(number);
+    //     console.log(formattedNumber)
 
-      //  Show the number in the #show-number tag.
-      $("#show-number").html("<h2>" + number + "</h2>");
+    //      //  Show the number in the #show-number tag.
+    //     $("#toothTimerButton").html(formattedNumber.minute+":"+formattedNumber.seconds);
+        
+    //     //  Once number hits zero...
+    //     if (number === 0) {
+    //         stop();
+    //         //  Alert the user that time is up.
+    //         alert("Time Up!");
+    //     }
+    // }
+    // //  The stop function
+    // function stop() {
+    //     clearInterval(intervalId);
+    // }
+
+});
+
+  
 
 
-      //  Once number hits zero...
-      if (number === 0) {
 
-        //  ...run the stop function.
-        stop();
-      }
-    }
 
-    //  The stop function
-    function stop() {
 
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(intervalId);
-    }
 
-    //  Execute the run function.
-    run();
 
-})
+
 
 
 $('.todos').on('click','a',function(){
