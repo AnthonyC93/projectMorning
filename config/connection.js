d49e019bce15fb26d9d2fd5029dd8bfa822c7dbc
acarta <anthonym.cartagena@gmail.com>
2019-01-12 01:28:10 -0500

    configured for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 //export the connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "morningApp"
-});
+if(process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "morningApp"
+    });
+}
 
 connection.connect(function(err){
     if(err)throw err;
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS morningApp;
 CREATE DATABASE morningApp;
 USE morningApp;
 
